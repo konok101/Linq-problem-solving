29c2a2cdab0bb21cb814164c4b0796eafb97bda4
Administrator <Administrator@ITHO-WS-KONOK>
2023-11-19 14:51:59 +0600

    some query added for same condition use select, from, in, where

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Linq
@@ -7,16 +8,58 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 5, 4, 1, 3, 9, 8 };
-            var numsPlusOne = from n in numbers select n;
-            for(int i=0; i<numbers.Length; i++)
+            int[] numbers =  { 5, 4, 1, 3, 9, 8 };
+
+
+
+            //Step1: Data Source
+            List<int> integerList = new List<int>()
             {
-                Console.WriteLine(numbers[i]);
-            }
-            foreach (var i in numsPlusOne)
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            //Step2: Query
+            //LINQ Query using Query Syntax to fetch all numbers which are > 5
+            var QuerySyntax = from obj in numbers //Data Source
+                              where obj > 5 //Condition
+                              select obj; //Selection
+            //Step3: Execution
+            foreach (var item in QuerySyntax)
             {
-                Console.WriteLine(i.ToString());
+                Console.Write(item + " ");
             }
+            Console.ReadKey();
+
+
+
+
+
+
+
+            var res = from a in numbers where a < 5 orderby a select a;
+            foreach (int i in res)
+            Console.WriteLine(i);
+            Console.WriteLine("\n");
+
+
+            int[] nums = new int[] { 0, 1, 2 };
+            var ress = from a in nums where a < 1 orderby a select a;
+            foreach (int i in ress)
+                Console.WriteLine(i);
+
+
+
+            /*   var numsPlusOne = from n in numbers select n;
+               for(int i=0; i<numbers.Length; i++)
+               {
+                   Console.WriteLine(numbers[i]);
+               }
+               foreach (var i in numsPlusOne)
+               {
+                   Console.WriteLine(i.ToString());
+               }
+
+               */
+
         }
     }
 }
