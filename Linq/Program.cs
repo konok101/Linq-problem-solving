b3b747be8ff55b6f8ba39248460d0d7a89296fac
Administrator <Administrator@ITHO-WS-KONOK>
2023-11-19 13:56:43 +0600

    Add project files.

---
+++ Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8 };
+            var numsPlusOne = from n in numbers select n;
+            for(int i=0; i<numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            foreach (var i in numsPlusOne)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
